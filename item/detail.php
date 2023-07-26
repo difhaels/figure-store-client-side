eacf88f7bc162982a6b420cf17875d3dafad3c8f
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-26 11:25:29 +0700

    add fitur detail v1

--- item/detail.php
+++ item/detail.php
@@ -32,11 +32,61 @@ $item = read("SELECT * FROM item WHERE id = $id")[0];
         </div>
     </div>
 
-    <div class="pt-24 bg-red-300">
-        <div>
-            <img src="../img/item/<?= $item['image'] ?>" alt="<?= $item['name'] ?>" class="h-40 mx-auto">
+    <div class="pt-28 flex">
+        <div class="lg:w-1/2">
+            <img src="../img/item/<?= $item['image'] ?>" alt="<?= $item['code'] ?>" class="h-56 mx-auto">
+            <div class="flex justify-center gap-2 mt-2">
+                <img src="../img/sub/<?= $item['image1'] ?>" alt="<?= $item['code'] ?>1" class="h-56">
+                <img src="../img/sub/<?= $item['image2'] ?>" alt="<?= $item['code'] ?>2" class="h-56">
+            </div>
+            <div class="flex justify-center gap-2 mt-2">
+                <img src="../img/sub/<?= $item['image3'] ?>" alt="<?= $item['code'] ?>3" class="h-56">
+                <img src="../img/sub/<?= $item['image4'] ?>" alt="<?= $item['code'] ?>4" class="h-56">
+            </div>
         </div>
-        <div></div>
+        <div class="lg:w-1/2 pt-[50px]">
+
+            <h1 class="text-xl"><?= $item['source'] ?> - <?= $item['name'] ?> - <?= $item['type'] ?> #<?= $item['code'] ?></h1>
+            <a href="">
+                <div class="mt-3 bg-[#E7230D] w-[210px] rounded-lg text-white font-semibold px-5 py-3 flex justify-between">
+                    <h1>Buy Now</h1>
+                    <h1>Rp <?= number_format($item['price'], 0, ',', '.'); ?></h1>
+                </div>
+            </a>
+
+            <div class="mt-7 px-5 border border-black rounded-lg w-fit bg-[#EEEDED]">
+                <h1 class="text-xl font-bold py-3">Transaction Flow</h1>
+                <div class="flex gap-3 pb-5">
+                    <div class="w-[160px] bg-white rounded-lg flex items-center justify-center gap-3 py-2">
+                        <img src="../img/icon/transfer.png" alt="transfer" class="h-10">
+                        <h1>Transfer</h1>
+                    </div>
+                    <div class="w-[160px] bg-white rounded-lg flex items-center justify-center gap-3 py-2">
+                        <img src="../img/icon/send.png" alt="send" class="h-10">
+                        <h1>Send</h1>
+                    </div>
+                    <div class="w-[160px] bg-white rounded-lg flex items-center justify-center gap-3 py-2">
+                        <img src="../img/icon/receive.png" alt="receive" class="h-10">
+                        <h1>Receive</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pt-7 flex gap-10">
+                <div>
+                    <h1 class="text-slate-400">Type</h1>
+                    <h1><?= $item['type'] ?></h1>
+                </div>
+                <div>
+                    <h1 class="text-slate-400">Dimensions</h1>
+                    <h1><?= $item['dimensions'] ?></h1>
+                </div>
+                <div>
+                    <h1 class="text-slate-400">Material</h1>
+                    <h1><?= $item['material'] ?></h1>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
